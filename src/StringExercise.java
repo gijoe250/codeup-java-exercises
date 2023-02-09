bf84c25caaacfb10ab0638b033fa038ad2ff96c0
Joe Garcia <gijoe250@gmail.com>
2023-02-09 14:26:23 -0600

    adding first string exercise completed

---
+++ src/StringExercise.java
@@ -0,0 +1,27 @@
+public class StringExercise {
+    public static void main(String[] args){
+//        For each of the following output examples: create a String variable that contains the desired output and print it out to the console,
+//        you can do this with only one String variable and one print statement for each output example.
+//
+//
+//                We don't need no education
+//        We don't need no thought control
+        String rebel = "We don't need no education\n" +
+                "We don't need no thought control\n";
+        System.out.println(rebel);
+//
+//        Check "this" out!, "s inside of "s!
+        String quotes = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(quotes);
+//
+//                In windows, the main drive is usually C:\
+        String drive = "In windows, the main drive is usually C:\\\n";
+        System.out.println(drive);
+//
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+        String crazy = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(crazy);
+    }
+}
